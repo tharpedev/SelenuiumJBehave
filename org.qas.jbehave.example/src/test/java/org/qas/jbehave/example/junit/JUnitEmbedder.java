9f2270fa5721243db3c6212c4700a9ac3f47065c
Phu Ta <phuta@qasymphony.com>
2019-01-08 09:57:56 +0700

    upgrade plugin version to run with java 10+

--- org.qas.jbehave.example/src/test/java/org/qas/jbehave/example/junit/JUnitEmbedder.java
+++ org.qas.jbehave.example/src/test/java/org/qas/jbehave/example/junit/JUnitEmbedder.java
@@ -1,24 +1,12 @@
 package org.qas.jbehave.example.junit;
 
-import com.qasymphony.qtest.automation.plugin.jbehave.core.LogCollector;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.embedder.EmbedderControls;
-import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.io.LoadFromRelativeFile;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
-import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.After;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.qas.jbehave.example.Utils.TestUtil;
-import org.qas.jbehave.example.steps.SimpleSearchSteps;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 
@@ -34,17 +22,6 @@ public class JUnitEmbedder extends Embedder {
         return new EmbedderControls().doIgnoreFailureInStories(true).doIgnoreFailureInView(true);
     }
 
-//    @Override
-//    public Configuration configuration() {
-//        Class<? extends JUnitEmbedder> embedderClass = this.getClass();
-//        return new MostUsefulConfiguration()
-//                .useStoryLoader(new LoadFromClasspath(embedderClass.getClassLoader()))
-//                .useStoryReporterBuilder(new StoryReporterBuilder()
-//                        .withReporters(new LogCollector())
-//                        .withFormats(Format.CONSOLE)
-//                        .withFormats(Format.HTML)
-//                        .withFormats(Format.XML));
-//    }
     @Override
     public Configuration configuration() {
         return  testUtil.configuration();
@@ -55,7 +32,6 @@ public class JUnitEmbedder extends Embedder {
         return testUtil.stepsFactory();
     }
 
-
     @After
     public void tearDown() throws Exception {
         testUtil.tearDown();
@@ -65,5 +41,4 @@ public class JUnitEmbedder extends Embedder {
     public void runStoriesAsPaths(List<String> storyPaths) {
         super.runStoriesAsPaths(testUtil.storyPaths());
     }
-
 }
